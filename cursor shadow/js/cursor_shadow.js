3155c1a37628c8d7b2eae47a3d4deec408a6b86c
Edu Poch <edupoch@gmail.com>
2011-07-11 09:09:00 +0200

    Trying to develop non intrusive Javascript code.

--- cursor shadow/js/cursor_shadow.js
+++ cursor shadow/js/cursor_shadow.js
@@ -1,7 +1,29 @@
 $(function() {
+    
+    $("body").append(
+    '<div id="cursor_far_shadow"><img src="./img/ZoomInShadow.png"></div>');
+    
+    $("div.floating_div")
+    .css({"z-index": "50",
+        "-webkit-box-shadow":"20px 20px 5px #999",
+        "overflow":"hidden"
+    })
+    .html('<div id="cursor_close_shadow"><img src="./img/ZoomInShadow.png"></div>');
        
-    cursor_far_shadow = $("#cursor_far_shadow");
-    cursor_close_shadow = $("#cursor_close_shadow");
+    cursor_far_shadow = $("#cursor_far_shadow");    
+    cursor_far_shadow.css({
+       "position": "absolute",
+        "z-index": "30",
+        "opacity": "0.5",
+        "display":"none"
+    });
+    
+    cursor_close_shadow = $("#cursor_close_shadow");    
+    cursor_close_shadow.css({
+        "z-index": "80",
+        "opacity": "1",
+        "display":"none"
+    });
 
     $("html").mousemove(function(e){
         cursor_far_shadow.css("margin-left",e.pageX+30);
